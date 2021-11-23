aeeda1994f639e105be5d55f34ece6ae045bb59b
kawakami1592 <57590363+kawakami1592@users.noreply.github.com>
2021-11-23 23:14:24 +0900

    完了

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,6 +6,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String _textHeader = "あなたの名前は？";
+  TextEditingController _textEditingController =
+      TextEditingController(); //入力された文字の処理を動的にするコントローラ
+  Widget _imageWidget = Container(); //初期値は空の箱
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,27 +20,52 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
       ),
       body: Container(
-        padding: EdgeInsets.all(8.0),//端から全方向に8.0のpadding
+        //ただの箱
+        padding: EdgeInsets.all(8.0), //端から全方向に8.0のpadding
         child: Column(
           //子要素を縦に並べる
           children: [
-            Center(child: Text("名前を入れてね", style: TextStyle(fontSize: 25.0))),
-            TextField(
-              keyboardType: TextInputType.text,
-              style: TextStyle(fontSize: 20.0),
-              controller: null, //todo
+            Center(
+              child: Text(
+                _textHeader,
+                style: TextStyle(fontSize: 25.0),
+              ),
             ),
-            Expanded(child: Image.asset("assets/images/hello.png")),
+            //子要素を中央よせに
+            TextField(
+              //入力欄
+              keyboardType: TextInputType.text, //キーボードのタイプ
+              style: TextStyle(fontSize: 20.0),
+              controller: _textEditingController, //入力された文字の処理
+            ),
+            Expanded(child: _imageWidget),
+            //子要素を引き伸ばす
             SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                    onPressed: null,
-                    child: Text("おしねて", style: TextStyle(fontSize: 20.0))
-                )
-            )
+              //サイズをもったBox
+              width: double.infinity, //親の幅いっぱい
+              child: ElevatedButton(
+                //ボタン
+                onPressed: () => _onClick(), //ボタンを押した時の処理
+                child: Text(
+                  "おしねて",
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
+  }
+
+  _onClick() {
+    //ボタンを押した時の処理
+    setState(() {
+      //値をセットしbuild()を動かす
+      var inputText = _textEditingController.text; //ローカル変数　入力された文字を取得
+      _textHeader = "こんにちは!$inputTextさん";
+      _imageWidget =
+          Image.asset("assets/images/hello.png"); //assetフォルダの中の画像を表示する
+    });
   }
 }
